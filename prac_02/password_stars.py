d34b626734c44ad77d90d3b86c901359af124467
Takuma <takuma.matsunami@my.jcu.edu.au>
2026-06-01 20:21:43 +1000

    Edit password_stars.py into functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,7 +2,23 @@
 write a program that asks the user for a password, with error-checking to repeat if the password doesn't meet a minimum length set by a variable.
 """
 
-password = input("Password: ")
-for character in password:
-    print("*", end="")
 
+def main():
+    password = get_password()
+    print_starts(password)
+
+
+def get_password():
+    password = input("Password: ")
+    while password == "":
+        print("No empty")
+        password = input("Password: ")
+    return password
+
+
+def print_starts(password):
+    for character in password:
+        print("*", end="")
+
+
+main()
