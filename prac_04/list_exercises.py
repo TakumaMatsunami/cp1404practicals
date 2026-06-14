ffc5a290ba54616b25272f47b091a28e6fe114fa
Takuma <takuma.matsunami@my.jcu.edu.au>
2026-06-15 09:23:28 +1000

    Add on to list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -16,3 +16,14 @@ def main():
 
 
 main()
+
+# Woefully inadequate security checker
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
