7e77289d541b946342c46d11852b53a280248017
Takuma <takuma.matsunami@my.jcu.edu.au>
2026-06-23 14:32:15 +1000

    Add emails.py into prac_05

---
+++ prac_05/emails.py
@@ -0,0 +1,25 @@
+"""
+Word Occurrences
+Estimate: 30 minutes
+Actual:  minutes
+"""
+name_to_email = {}
+
+email = input("Email: ")
+while email != "":
+    name = email.split("@")
+    name = name[0].split(".")
+    for i, word in enumerate(name):
+        name[i] = name[i].title()
+    name = " ".join(name)
+    is_name = input(f"Is your name {name}? (Y/n) ").lower()
+    if is_name == "y" or is_name == "":
+        name_to_email[name] = email
+    else:
+        name = input("Name: ")
+        name_to_email[name] = email
+    email = input("Email: ")
+print()
+
+for name, email in name_to_email.items():
+    print(f"{name} ({email})")
