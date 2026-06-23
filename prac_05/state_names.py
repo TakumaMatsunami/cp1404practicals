38f01dc6793d7e2b55b8717c4f2cc51f75d9c78f
Takuma <takuma.matsunami@my.jcu.edu.au>
2026-06-23 13:52:37 +1000

    Add state_names.py into prac_05

---
+++ prac_05/state_names.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+
+# print(CODE_TO_NAME)
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+print()
+
+state_code = input("Enter short state: ").upper()
+
+# EAFP
+while state_code != "":
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    except KeyError:
+        print("Invalid short state")
+
+# LBYL
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
